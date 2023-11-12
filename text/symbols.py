1e728c23b6b1c8f694dab2d623294890da39799d
GalaxyCong <109259667+GalaxyCong@users.noreply.github.com>
2023-11-12 15:46:18 +0800

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -1,10 +1,3 @@
-""" from https://github.com/keithito/tacotron """
-
-"""
-Defines the set of symbols used in text input to the model.
-
-The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. """
-
 from text import cmudict, pinyin
 
 _pad = "_"
@@ -19,11 +12,6 @@ _pinyin = ["@" + s for s in pinyin.valid_symbols]
 
 # Export all symbols:
 symbols = (
-    [_pad]
-    + list(_special)
-    + list(_punctuation)
-    + list(_letters)
-    + _arpabet
-    + _pinyin
+    _arpabet
     + _silences
 )
